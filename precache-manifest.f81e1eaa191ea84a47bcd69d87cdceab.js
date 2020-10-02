cf7f3bd46967f0d7e05143e284420818c770c3e8
Kostyazh <zhebelev.konstantin@yandex.ua>
2020-10-02 16:40:57 +0300

    Updates

---
+++ precache-manifest.f81e1eaa191ea84a47bcd69d87cdceab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c92e5b6e0a2ed1e506eb",
+    "url": "/Resume/static/css/main.a422c397.chunk.css"
+  },
+  {
+    "revision": "c92e5b6e0a2ed1e506eb",
+    "url": "/Resume/static/js/main.c92e5b6e.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/Resume/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "8d5efb982c82b6672434",
+    "url": "/Resume/static/js/runtime~main.8d5efb98.js"
+  },
+  {
+    "revision": "cc7a096428a32834ceee6210234d1e7f",
+    "url": "/Resume/index.html"
+  }
+];
